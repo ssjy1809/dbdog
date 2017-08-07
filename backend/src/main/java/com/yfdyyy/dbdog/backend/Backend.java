7e3a99c62a86cbf31252d79007141d3c4ba751ed
ZhongJoker <zhonghua@weidian.com>
2017-08-07 22:52:10 +0800

    dev

---
+++ backend/src/main/java/com/yfdyyy/dbdog/backend/Backend.java
@@ -0,0 +1,7 @@
+package com.yfdyyy.dbdog.backend;
+
+/**
+ * Created by zhongjoker on 2017/8/7.
+ */
+public class Backend {
+}
---
+++ sequence/src/main/java/com/yfdyyy/dbdog/sequence/Sequencer.java
@@ -0,0 +1,7 @@
+package com.yfdyyy.dbdog.sequence;
+
+/**
+ * Created by zhongjoker on 2017/8/7.
+ */
+public class Sequencer {
+}
